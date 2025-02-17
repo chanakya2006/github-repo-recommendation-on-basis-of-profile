17805d6e1defe55b31ff9ff508cf333c0e48b6c7
ChanakyaHarit <95040417+ChanakyaHarit@users.noreply.github.com>
2025-02-18 04:27:34 +0530

    Update webscraping.py

--- webscraping.py
+++ webscraping.py
@@ -49,20 +49,33 @@ def get_repo_readme(target_repo_urls : list) -> dict:
 
 def get_commits_from_repo_url(link_to_repo: str):
     driver.get(link_to_repo)
-    time.sleep(2)
+    time.sleep(1)
     temp = driver.find_elements(By.TAG_NAME,"a")
     for i in temp:
         if i.get_attribute("class") == "prc-Button-ButtonBase-c50BI LinkButton-module__code-view-link-button--xvCGA flex-items-center fgColor-default":
             driver.get(i.get_attribute("href"))
             break
+    time.sleep(2)
     soup = BeautifulSoup(driver.page_source,"html.parser")
+    dic = {}
     for i in soup.find_all("div",class_ = "Timeline__ToggleTimelineItem-sc-1nkzbnu-1 ehuczD Timeline-Item"):
         for k in i.find_all("a",class_ = "prc-Link-Link-85e08"):
-            print(k.get("href"))
-    print("Next =================")
+            if k.get("class") != ['Box-sc-g0xbh4-0', 'epISVM', 'prc-Link-Link-85e08']:
+                print(k.get("href"))
+                if k.get("href") not in dic:
+                    dic[k.get("href")] = 1
+                else :
+                    dic[k.get("href")] += 1
     for i in soup.find_all("div",class_ = "Timeline__ToggleTimelineItem-sc-1nkzbnu-1 bTwOen Timeline-Item"):
         for k in i.find_all("a",class_ = "prc-Link-Link-85e08"):
-            print(k.get("href"))
-    # print(soup.find_all("div",class_ = "Timeline__ToggleTimelineItem-sc-1nkzbnu-1 ehuczD Timeline-Item")[0].find("a",class_ = "prc-Link-Link-85e08").get("href"))
+            if k.get("class") != ['Box-sc-g0xbh4-0', 'epISVM', 'prc-Link-Link-85e08']:
+                print(k.get("href"))
+                if k.get("href") not in dic:
+                    dic[k.get("href")] = 1
+                else :
+                    dic[k.get("href")] += 1
 
-get_commits_from_repo_url("https://github.com/chanakya2006/github-repo-recommendation-on-basis-of-profile")
+    # error occurs at https://github.com/chanakya2006/github-repo-recommendation-on-basis-of-profile/commits/main/ check on this repo
+    return dic
+
+get_commits_from_repo_url("https://github.com/DoingWoW/buildWow/commits/master/")

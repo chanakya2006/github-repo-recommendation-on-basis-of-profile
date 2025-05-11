d413be25bffa57d8d875dc03c6f2a4fa6e4e3c82
ChanakyaHarit <165587139+chanakya2006@users.noreply.github.com>
2025-05-11 16:24:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, render_template, request
 from Content_based_Rec import GitHubRecommender
+from flask_cors import CORS
 
 # To run : python main.py
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
